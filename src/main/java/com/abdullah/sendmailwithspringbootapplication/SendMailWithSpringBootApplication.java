8ae27a809447d1406aa2b5c6324dc8c2afdccc4b
Abdullah Bakış <abdullahbakis1672@gmail.com>
2022-10-31 23:55:06 +0300

    Update

--- src/main/java/com/abdullah/sendmailwithspringbootapplication/SendMailWithSpringBootApplication.java
+++ src/main/java/com/abdullah/sendmailwithspringbootapplication/SendMailWithSpringBootApplication.java
@@ -1,13 +1,62 @@
 package com.abdullah.sendmailwithspringbootapplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class SendMailWithSpringBootApplication {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public static void main(String[] args) {
         SpringApplication.run(SendMailWithSpringBootApplication.class, args);
     }
 
+    public void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("email address to whom you send the mail");
+
+        msg.setSubject("Testing from Spring Boot");
+        msg.setText("Hello World \n Spring Boot Email");
+        javaMailSender.send(msg);
+
+    }
+
+    public void sendEmailWithAttachment() throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+
+        helper.setTo("email address to whom you send the mail");
+
+        helper.setSubject("Testing from Spring Boot");
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>Check attachment for image!</h1>", true);
+
+        // hard coded a file path
+        FileSystemResource file = new FileSystemResource(new File("D:\\Duvar Kağıtları\\Yildizlar"));
+
+        helper.addAttachment("Yildizlar", file);
+
+        javaMailSender.send(msg);
+
+    }
 }
--- src/main/java/com/abdullah/sendmailwithspringbootapplication/controller/SendEmailController.java
+++ src/main/java/com/abdullah/sendmailwithspringbootapplication/controller/SendEmailController.java
@@ -1,2 +1,24 @@
-package com.abdullah.sendmailwithspringbootapplication.controller;public class SendEmailController {
+package com.abdullah.sendmailwithspringbootapplication.controller;
+
+import com.abdullah.sendmailwithspringbootapplication.SendMailWithSpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import java.io.IOException;
+
+@RestController
+public class SendEmailController {
+
+    @Autowired
+    SendMailWithSpringBootApplication sendMailWithSpringBootApplication;
+
+    @RequestMapping(value = "/sendemail")
+    public String send() throws AddressException, MessagingException, IOException {
+        //sendEmail();
+        sendMailWithSpringBootApplication.sendEmail();
+        return "Email sent successfully";
+    }
 }
